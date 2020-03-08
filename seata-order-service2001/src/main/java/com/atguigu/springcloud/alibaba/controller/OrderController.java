aae97741d97cf6410cff90b572cdac52d91443e4
leelovejava <1165746141@qq.com>
2020-03-08 21:52:56 +0800

    面试第三季之雪花算法

--- seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/controller/OrderController.java
+++ seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/controller/OrderController.java
@@ -2,11 +2,14 @@ package com.atguigu.springcloud.alibaba.controller;
 
 import com.atguigu.springcloud.alibaba.domain.CommonResult;
 import com.atguigu.springcloud.alibaba.domain.Order;
+import com.atguigu.springcloud.alibaba.service.IdGeneratorSnowflake;
 import com.atguigu.springcloud.alibaba.service.OrderService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author zzyy
@@ -16,10 +19,35 @@ import javax.annotation.Resource;
 public class OrderController {
     @Resource
     private OrderService orderService;
+    @Resource
+    private IdGeneratorSnowflake idGeneratorSnowflake;
 
+    /**
+     * 创建订单
+     *
+     * @param order
+     * @return
+     */
     @GetMapping("order/create")
     public CommonResult create(Order order) {
         orderService.create(order);
         return new CommonResult(200, "订单创建成功");
+    }
+
+    /**
+     * 生成id,通过雪花算法
+     *
+     * @return
+     */
+    @GetMapping("snowflake")
+    public String getIDBySnowflake() {
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 20; i++) {
+            threadPool.submit(() -> {
+                System.out.println(idGeneratorSnowflake.snowflakeId());
+            });
+        }
+        threadPool.shutdown();
+        return "hello snowflake";
     }
 }
---
+++ seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/service/IdGeneratorSnowflake.java
@@ -0,0 +1,48 @@
+package com.atguigu.springcloud.alibaba.service;
+
+import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.net.NetUtil;
+import cn.hutool.core.util.IdUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author zzyy
+ * @date 2020/3/8 18:59
+ **/
+@Slf4j
+@Component
+public class IdGeneratorSnowflake {
+    private long workerId = 0;
+    private long datacenterId = 1;
+    private Snowflake snowflake = IdUtil.createSnowflake(workerId, datacenterId);
+
+    @PostConstruct
+    public void init() {
+        try {
+            workerId = NetUtil.ipv4ToLong(NetUtil.getLocalhostStr());
+            log.info("当前机器的workerId:{}", workerId);
+        } catch (Exception e) {
+            log.info("当前机器的workerId获取失败", e);
+            workerId = NetUtil.getLocalhostStr().hashCode();
+            log.info("当前机器 workId:{}", workerId);
+        }
+
+    }
+
+    public synchronized long snowflakeId() {
+        return snowflake.nextId();
+    }
+
+    public synchronized long snowflakeId(long workerId, long datacenterId) {
+        snowflake = IdUtil.createSnowflake(workerId, datacenterId);
+        return snowflake.nextId();
+    }
+
+    public static void main(String[] args) {
+        // 1236610764324864000
+        System.out.println(new IdGeneratorSnowflake().snowflakeId());
+    }
+}
