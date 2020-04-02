f65b00f29f044c6a29d90a99bd4cba5bfed1538e
leelovejava <1165746141@qq.com>
2020-04-02 12:09:43 +0800

    fix refresh写错

--- cloud-config-center-3344/src/main/resources/bus-refresh.cmd
+++ cloud-config-center-3344/src/main/resources/bus-refresh.cmd
@@ -4,4 +4,4 @@ curl +X POST "http://config-3344.com:3344/actuator/bus-refresh"
 # 公式 http://配置中心ip:配置中心端口/actuator/bus-refresh/{destination}
 # destination = 微服务名称：端口号
 # 定点通知
-curl +X POST "http://config-3344.com:3344/actuator/bus-refrensh/config-client:3355"
+curl +X POST "http://config-3344.com:3344/actuator/bus-refresh/config-client:3355"
